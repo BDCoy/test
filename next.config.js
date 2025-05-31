57e95b8049d25c08dd9039d26470c653f7f5a098
Brayan David <65578489+BDCoy@users.noreply.github.com>
2025-05-31 10:09:22 -0400

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
